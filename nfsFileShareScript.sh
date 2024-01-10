6667ed01a9192dd66a93bfc0536d6a418a384747
Zhelus <zhelus@rocketmail.com>
2024-01-10 13:56:56 -0600

    Update nfsFileShareScript.sh

--- nfsFileShareScript.sh
+++ nfsFileShareScript.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-#this script should be run as the cluster service account
-#This allows for direct ssh to any node in the cluster without the use of the sshpass protocol
-
-#-s suppress user input (don't show typed password) -p print to terminal what is in parethesis
-#read -p "Enter management account:" acct
-
 
 LDAPsearchBase=$(sudo cat /etc/sssd/LDAPsearchBase)
 binddnAcct=$(sudo cat /etc/sssd/binddnAcct)
@@ -12,7 +6,6 @@ binddnAcct=$(sudo cat /etc/sssd/binddnAcct)
 binddnPW=$(sudo cat /etc/sssd/binddnPW)
 SG=$(sudo cat /etc/sssd/securityGroup)
 LDAPserver=$(sudo cat /etc/sssd/LDAPserver)
-
 
 ldapsearch -H "$LDAPserver" -x -D "$binddnAcct" -w "$binddnPW" -b "$LDAPsearchBase" "$SG" | grep member: | awk -F'CN=' '{print $2}' | awk -F',' '{print $1}' | awk '{print tolower($0)}' > groupMembership
 
@@ -25,4 +18,3 @@ do
 	echo "/home/$i *(rw,sync,subtree_check)" | sudo tee -a /etc/exports	
 done
 rm groupMembership
-
