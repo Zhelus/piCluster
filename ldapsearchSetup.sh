3f35aeb917127f430f8051cd1fceaa92ec3837da
zhelus <zhelus@rocketmail.com>
2024-01-17 22:52:38 -0600

    Modified nfs query for LDAP environment

--- ldapsearchSetup.sh
+++ ldapsearchSetup.sh
@@ -4,4 +4,4 @@ sudo cat /etc/sssd/sssd.conf | grep ldap_search_base | awk -F ',' '{print $2","$
 sudo cat /etc/sssd/sssd.conf | grep ldap_uri | awk -F 'ldap_uri = ' '{print $2}' | sudo tee /etc/sssd/LDAPserver >/dev/null
 sudo cat /etc/sssd/sssd.conf | grep ldap_default_bind_dn | awk -F 'ldap_default_bind_dn = ' '{print $2}' | sudo tee /etc/sssd/binddnAcct >/dev/null
 sudo cat /etc/sssd/sssd.conf | grep ldap_default_authtok | awk -F 'ldap_default_authtok = ' '{print $2}' | sudo tee /etc/sssd/binddnPW >/dev/null
-sudo cat /etc/sssd/sssd.conf | grep ldap_access_filter | awk -F 'ldap_access_filter = memberOf=' '{print $2}' | awk -F ',' '{print "("$1")"}' | sudo tee /etc/sssd/securityGroup >/dev/null
+#sudo cat /etc/sssd/sssd.conf | grep ldap_access_filter | awk -F 'ldap_access_filter = memberOf=' '{print $2}' | awk -F ',' '{print "("$1")"}' | sudo tee /etc/sssd/securityGroup >/dev/null
--- nfsFileShareScript.sh
+++ nfsFileShareScript.sh
@@ -4,13 +4,14 @@ LDAPsearchBase=$(sudo cat /etc/sssd/LDAPsearchBase)
 binddnAcct=$(sudo cat /etc/sssd/binddnAcct)
 #yes there the -Y flag to point to the password file, but I want it owned by root so this script doesn't have to be run elevated
 binddnPW=$(sudo cat /etc/sssd/binddnPW)
-SG=$(sudo cat /etc/sssd/securityGroup)
 LDAPserver=$(sudo cat /etc/sssd/LDAPserver)
+#SG=$(sudo cat /etc/sssd/securityGroup)
+SG=$(awk '/#beginSGconfig/{flag=1; next} /#endSGconfig/{flag=0} flag' /etc/opt/piLab/masterConfig)
 
-ldapsearch -H "$LDAPserver" -x -D "$binddnAcct" -w "$binddnPW" -b "$LDAPsearchBase" "$SG" | grep member: | awk -F'CN=' '{print $2}' | awk -F',' '{print $1}' | awk '{print tolower($0)}' > groupMembership
+ldapsearch -H "$LDAPserver" -x -D "$binddnAcct" -w "$binddnPW" -b "$LDAPsearchBase" "(cn=$SG)" | grep member: | awk -F'CN=' '{print $2}' | awk -F',' '{print $1}' | awk '{print tolower($0)}' > groupMembership
 
 members=$(cat "groupMembership")
-
+echo $SG
 for i in $members
 do
 	sudo mkdir /home/$i
